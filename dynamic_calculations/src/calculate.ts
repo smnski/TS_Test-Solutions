5cce1e600ab897ac9ded39f752a2f3db141a11b6
avren <szymonurbanski217@gmail.com>
2025-03-13 17:46:04 +0100

    newest finds correct timestamp, but returns only it instead of action object now.

--- dynamic_calculations/src/calculate.ts
+++ dynamic_calculations/src/calculate.ts
@@ -7,21 +7,16 @@ async function calculate(event: EventPayload) {
   const headers = event.Headers;
   const body = event.body;
 
-  console.log("body: ", body); //debug
-
   const userid = headers.userid;
   const { actionid } = JSON.parse(body);
 
-  console.log("actionid: ", actionid); //debug
-
   const action = await Action.getByPk(actionid);
-  console.log("action: ", action); //debug
 
   const childrenActions = await action.getChildActions();
   console.log("childrenActions: ", childrenActions); //debug
 
-  const allActions = await dbClient.scan({ TableName: TableNames.actions }).promise();
-  console.log("All actions in DB:", JSON.stringify(allActions, null, 2));
+  const motherHandleResult = await action.handler.handle(childrenActions);
+  console.log("motherHandleResult: ", motherHandleResult); //debug
 
   return {};
 }
--- dynamic_calculations/src/handlers/newest.ts
+++ dynamic_calculations/src/handlers/newest.ts
@@ -2,6 +2,7 @@ class Newest {
 
   static handle(...sources) {
     return sources
+    .flat()
     .map(source => source.data.timestamp)
     .reduce((latest, current) => (latest > current ? latest : current), new Date(0));
   }
