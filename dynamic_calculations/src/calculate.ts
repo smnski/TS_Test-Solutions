f45f52b54b203c3b9a92be4707b306ecae431ae4
avren <szymonurbanski217@gmail.com>
2025-03-14 00:39:19 +0100

    Working on recursive processing

--- dynamic_calculations/src/calculate.ts
+++ dynamic_calculations/src/calculate.ts
@@ -1,23 +1,13 @@
 import { EventPayload } from "./types";
 import { Action } from "./models/action";
-import { dbClient, TableNames } from  "./common/db";
 
 async function calculate(event: EventPayload) {
-
-  const headers = event.Headers;
-  const body = event.body;
-
-  const userid = headers.userid;
+  const { Headers, body } = event;
   const { actionid } = JSON.parse(body);
-
   const action = await Action.getByPk(actionid);
-
-  const childrenActions = await action.getChildActions();
-  console.log("childrenActions: ", childrenActions); //debug
-
-  const motherHandleResult = await action.handler.handle(childrenActions);
-  console.log("motherHandleResult: ", motherHandleResult); //debug
-
+  const children = await action.getChildActions();
+  console.log("children: ", children); //debug
+  
   return {};
 }
 
--- dynamic_calculations/src/models/action.ts
+++ dynamic_calculations/src/models/action.ts
@@ -1,5 +1,4 @@
 import Role from "./role";
-
 import { dbClient, TableNames } from "../common/db";
 import HandlerAssigner from "../handlers/handlerAssigner";
 
@@ -9,6 +8,7 @@ export class Action {
   role;
   handler;
   data;
+  result;
 
   constructor(input) {
     this.pk = input.pk;
@@ -16,35 +16,27 @@ export class Action {
     this.role = Role.from(input.role);
     this.handler = HandlerAssigner.from(input.handler);
     this.data = input.data;
+    this.result = null;
   }
 
   static async getByPk(pk) {
-    const res = await dbClient.get({ TableName: TableNames.actions, Key: { pk: pk } }).promise();
-
+    const res = await dbClient.get({ TableName: TableNames.actions, Key: { pk } }).promise();
     if (!res.Item) {
       throw new Error("Action does not exist");
     }
-
     return new Action(res.Item);
   }
 
   async getChildActions() {
-    const res = await dbClient
-      .query({
-        TableName: TableNames.actions,
-        IndexName: "parent-index",
-        KeyConditionExpression: 'parentPk = :parentPk',
-        ExpressionAttributeValues: {
-          ':parentPk': this.pk,
-        },
-      })
-      .promise();
-  
+    const res = await dbClient.query({
+      TableName: TableNames.actions,
+      IndexName: "parent-index",
+      KeyConditionExpression: 'parentPk = :parentPk',
+      ExpressionAttributeValues: { ':parentPk': this.pk }
+    }).promise();
     if (!res.Items || res.Items.length === 0) {
       return [];
     }
-  
     return res.Items.map(item => new Action(item));
   }
-
 }
