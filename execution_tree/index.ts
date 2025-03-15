33c7ea37ac1e4b8ea49b89b45039c4316412ccf5
avren <szymonurbanski217@gmail.com>
2025-03-15 07:59:50 +0100

    Split into functions

--- execution_tree/index.ts
+++ execution_tree/index.ts
@@ -1,22 +1,34 @@
 import * as fs from "fs";
 import * as path from "path";
 
-const args = process.argv.slice(2);
-
-if (args.length === 0) {
-  console.error("Usage: ts-node index.ts <filename>");
-  process.exit(1);
+function readData(filename: string): string {
+  const filepath = path.resolve(filename);
+  try {
+    return fs.readFileSync(filepath, "utf-8");
+  } catch (err) {
+    console.error(`Error reading file: ${(err as Error).message}`);
+    process.exit(1);
+  }
 }
 
-const filename = args[0];
-const filepath = path.resolve(filename);
+function processData(data: string) {
+  return 1;
+}
 
-fs.readFile(filepath, "utf-8", (err, data) => {
-  if (err) {
-    console.error(`Error reading file: ${err.message}`);
+function main() {
+  const args = process.argv.slice(2);
+
+  if (args.length === 0) {
+    console.error("Usage: ts-node index.ts <filename>");
     process.exit(1);
   }
 
-  console.log("File content:");
-  console.log(data);
-});
+  const filename = args[0];
+  const data = readData(filename);
+  if (data) {
+    const res = processData(data);
+    console.log("Result: ", res);
+  }
+}
+
+main();
