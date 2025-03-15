91da3305bea8b4a73ca32e0f6957320372f0feba
avren <szymonurbanski217@gmail.com>
2025-03-15 07:46:21 +0100

    Reading data from file works

--- execution_tree/index.ts
+++ execution_tree/index.ts
@@ -1,1 +1,22 @@
-// YOUR CODE HERE
+import * as fs from "fs";
+import * as path from "path";
+
+const args = process.argv.slice(2);
+
+if (args.length === 0) {
+  console.error("Usage: ts-node index.ts <filename>");
+  process.exit(1);
+}
+
+const filename = args[0];
+const filepath = path.resolve(filename);
+
+fs.readFile(filepath, "utf-8", (err, data) => {
+  if (err) {
+    console.error(`Error reading file: ${err.message}`);
+    process.exit(1);
+  }
+
+  console.log("File content:");
+  console.log(data);
+});
