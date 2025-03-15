42f26af0d3897b0316fd8e509ebb7e47147e5fea
avren <szymonurbanski217@gmail.com>
2025-03-15 10:32:34 +0100

    Solution, slightly more complicated data to test.

--- execution_tree/index.ts
+++ execution_tree/index.ts
@@ -1,18 +1,81 @@
 import * as fs from "fs";
 import * as path from "path";
 
-function readData(filename: string): string {
+function readData(filename: string): string[] {
   const filepath = path.resolve(filename);
   try {
-    return fs.readFileSync(filepath, "utf-8");
+    return fs.readFileSync(filepath, "utf-8").split("\n").map(line => line.trim()).filter(line => line.length > 0);
   } catch (err) {
     console.error(`Error reading file: ${(err as Error).message}`);
     process.exit(1);
   }
 }
 
-function processData(data: string) {
-  return 1;
+function processData(lines: string[]) {
+
+  function doMath(action, leftVal, rightVal) {
+    let result = 0;
+    switch (action) {
+      case "+":
+        result = leftVal + rightVal;
+        break;
+      case "-":
+        result = leftVal - rightVal;
+        break;
+      case "*":
+        result = leftVal * rightVal;
+        break;
+      case "/":
+        result = leftVal / rightVal;
+        break;
+      case "^":
+        result = Math.pow(leftVal, rightVal);
+        break;
+      default:
+        throw new Error(`Unknown action: ${action}`);
+    }
+    return result;
+  }
+
+  const jsonString = lines.join("");
+  const expression = JSON.parse(jsonString);
+
+  const stack: any[] = [];
+  const evals: Map<object, number> = new Map();
+
+  stack.push(expression)
+
+  while (stack.length > 0) {
+    const node = stack.pop();
+
+    // Node has a pure value to save, no need for evaluation code below.
+    if ("value" in node) {
+      evals.set(node, node.value);
+      continue;
+    }
+
+    const { action, left, right } = node;
+    if (!evals.has(left)) {
+      stack.push(node);
+      // If left expression wasn't evaluated yet, add it on top of the stack to process in immediate iteration.
+      // Then return to main node, as it's just below on the stack.
+      stack.push(left);
+      continue;
+    }
+
+    if(!evals.has(right)) {
+      stack.push(node);
+      // If right expression wasn't evaluated yet, add it on top of the stack to process in immediate iteration.
+      // Then return to main node, as it's just below on the stack.
+      stack.push(right);
+      continue;
+    }
+
+    const result = doMath(action, evals.get(left), evals.get(right));
+    evals.set(node, result);
+  }
+  // Main expression now has a pure value set, with nothing left to evaluate.
+  return evals.get(expression);
 }
 
 function main() {
@@ -24,11 +87,9 @@ function main() {
   }
 
   const filename = args[0];
-  const data = readData(filename);
-  if (data) {
-    const res = processData(data);
-    console.log("Result: ", res);
-  }
+  const lines = readData(filename);
+  const result = processData(lines);
+  console.log("Result:", result);
 }
 
 main();
